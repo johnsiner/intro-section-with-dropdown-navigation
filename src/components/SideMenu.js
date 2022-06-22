7eee2c4db9ee712e617e9e9e16456938abe4e78b
Fabiyi Johnson <70953952+johnsiner@users.noreply.github.com>
2022-06-22 14:37:29 +0100

    Added transitioning to the mobile navigation

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -12,19 +12,17 @@ export default function SideMenu(props) {
    return (
       <Fragment>
          <Backdrop active={props.active} onClose={props.onClose} />
-         {props.active && (
-            <div className="side-menu">
-               <div className="close-menu-container">
-                  <img
-                     src={closeMenuIcon}
-                     alt="close-menu-icon"
-                     className="close-menu-icon"
-                     onClick={props.onClose}
-                  />
-               </div>
-               <NavbarSide />
+         <div className={`side-menu ${props.active && 'opened'}`}>
+            <div className="close-menu-container">
+               <img
+                  src={closeMenuIcon}
+                  alt="close-menu-icon"
+                  className="close-menu-icon"
+                  onClick={props.onClose}
+               />
             </div>
-         )}
+            <NavbarSide />
+         </div>
       </Fragment>
    );
 }
